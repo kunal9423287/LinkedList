f23d2ea6e3201047b47756ad23af85b10ba61897
Your Name <you@example.com>
2022-06-10 16:06:34 +0530

    Added:UC1

---
+++ src/LinkedList/Node.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class Node {
+
+	public static void main(String[] args)
+	{
+       System.out.println("Welcome to Data Structure");
+	   LinkedList linkedList = new LinkedList();
+	   linkedList.add(70);
+	   linkedList.add(30);
+	   linkedList.add(56);
+	   linkedList.print();
+
+	}
+
+}
